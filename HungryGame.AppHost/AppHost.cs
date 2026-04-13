1d22f47a8bd12d631f9cc651c59afd92e188f6cd
Jonathan Allen <jallen@snow.edu>
2026-04-13 14:25:37 -0600

    update apphost to deploy to azure app service

--- HungryGame.AppHost/AppHost.cs
+++ HungryGame.AppHost/AppHost.cs
@@ -6,31 +6,38 @@ var secretCode = builder.AddParameter("secretCode", secret: true);
 var massivePlayerCount = builder.AddParameter("massivePlayerCount", "5");
 var gameId = builder.AddParameter("gameId", secret: false);
 
+var appServiceEnvironment = builder.AddAzureAppServiceEnvironment("HungryGame-ASE");
+
 var hungrygame = builder.AddProject<Projects.HungryGame>("hungrygame")
-    .WithEnvironment("SECRET_CODE", secretCode);
+    .WithEnvironment("SECRET_CODE", secretCode)
+    .WithExternalHttpEndpoints()
+    .WithComputeEnvironment(appServiceEnvironment);
 
-builder.AddProject<Projects.massive>("massive")
-.WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
-.WithEnvironment("CLIENT_COUNT", massivePlayerCount)
-.WithEnvironment("GAME_ID", gameId)
-.WaitFor(hungrygame);
+if (!builder.ExecutionContext.IsPublishMode)
+{
+    builder.AddProject<Projects.massive>("massive")
+        .WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
+        .WithEnvironment("CLIENT_COUNT", massivePlayerCount)
+        .WithEnvironment("GAME_ID", gameId)
+        .WaitFor(hungrygame);
 
-builder.AddProject<Projects.foolhearty>("foolhearty")
-    .WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
-    .WithEnvironment("PLAY_STYLE", "Foolhearty")
-    .WithEnvironment("GAME_ID", gameId)
-    .WaitFor(hungrygame);
+    builder.AddProject<Projects.foolhearty>("foolhearty")
+        .WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
+        .WithEnvironment("PLAY_STYLE", "Foolhearty")
+        .WithEnvironment("GAME_ID", gameId)
+        .WaitFor(hungrygame);
 
-builder.AddProject<Projects.foolhearty>("foolhearty2")
-    .WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
-    .WithEnvironment("PLAY_STYLE", "Foolhearty")
-    .WithEnvironment("GAME_ID", gameId)
-    .WaitFor(hungrygame);
+    builder.AddProject<Projects.foolhearty>("foolhearty2")
+        .WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
+        .WithEnvironment("PLAY_STYLE", "Foolhearty")
+        .WithEnvironment("GAME_ID", gameId)
+        .WaitFor(hungrygame);
 
-builder.AddProject<Projects.foolhearty>("smartypants")
-    .WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
-    .WithEnvironment("PLAY_STYLE", "SmartyPants")
-    .WithEnvironment("GAME_ID", gameId)
-    .WaitFor(hungrygame);
+    builder.AddProject<Projects.foolhearty>("smartypants")
+        .WithEnvironment("SERVER", hungrygame.GetEndpoint("http"))
+        .WithEnvironment("PLAY_STYLE", "SmartyPants")
+        .WithEnvironment("GAME_ID", gameId)
+        .WaitFor(hungrygame);
+}
 
 builder.Build().Run();
